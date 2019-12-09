4789d7da1694123d54dba419736d29b8e5054e9f
ar-i-enterprise <ari2019.enterprise@gmail.com>
2019-12-09 18:13:32 +0100

    -first

---
+++ src/utils/Settings.js
@@ -0,0 +1,13 @@
+export const RESOLUTION_ARR = {
+  "120p,120p_1": [160, 120, 15, 65],
+  "360p_4": [640, 360, 30, 600],
+  "480p_4": [640, 480, 30, 750],
+  "720p_3": [1280, 720, 30, 1710]
+};
+
+export const SHARE_ID = 1;
+
+export const APP_ID = "7f5500af1525421591eb06bc49e3e3d1";
+
+// Assign Token to null if you have not enabled app certificate
+export const Token = "0067f5500af1525421591eb06bc49e3e3d1IAAl06YKI6MYGTZ5AkrBFeNlDPmCiDFoRhBdxCgYopIQEKPg45sAAAAAEAD+c/N2j7/vXQEAAQCQv+9d";
