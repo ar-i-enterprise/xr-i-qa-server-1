fa8d0ac8c690c926a9ec4d55d5d9b9abadc91746
ar-i-enterprise <ari2019.enterprise@gmail.com>
2019-12-10 04:13:14 +0100

    four

--- src/pages/meeting/meeting.js
+++ src/pages/meeting/meeting.js
@@ -209,9 +209,7 @@ const shareStart = () => {
         if (isChrome()) {
           // If (!chrome.app.isInstalled) {
           let msg = `
-            Please install chrome extension from 
-            <a href="https://chrome.google.com/webstore/detail/minllpmhdgpndnkomcoccfekfegnlikg">Google Webstore</a> 
-            before using sharing screen.   
+           "Share Screen Function is disabled !!!"I am working on this function. It will be implemented in the ner future  
           `;
           Notify.danger(msg, 5000);
           // }
@@ -248,6 +246,10 @@ const removeStream = id => {
   }
   Renderer.customRender(streamList, options.displayMode, mainId);
 };
+// my function--
+
+
+
 
 const addStream = (stream, push = false) => {
   let id = stream.getId();
@@ -565,13 +567,19 @@ const infoDetectSchedule = () => {
 // ------------- start --------------
 // ----------------------------------
 options = optionsInit();
+
 uiInit(options);
+
 // eslint-disable-next-line
+
 client = AgoraRTC.createClient({
   mode: options.transcode
 });
+
 subscribeMouseEvents();
+
 subscribeStreamEvents();
+
 clientInit(client, options).then(uid => {
   // Use selected device
   let config = isSafari()
@@ -607,3 +615,353 @@ clientInit(client, options).then(uid => {
 if (DUAL_STREAM_DEBUG) {
   setInterval(infoDetectSchedule, 1000);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import "babel-polyfill";
+import * as tf from "@tensorflow/tfjs";
+import * as tfd from "@tensorflow/tfjs-data";
+
+import { ControllerDataset } from "./controller_dataset";
+import * as ui from "./ui";
+
+// The number of classes we want to predict. In this example, we will be
+// predicting 4 classes for up, down, left, and right.
+const NUM_CLASSES = 4;
+
+// A webcam iterator that generates Tensors from the images from the webcam.
+let webcam;
+
+// The dataset object where we will store activations.
+const controllerDataset = new ControllerDataset(NUM_CLASSES);
+
+let truncatedMobileNet;
+let model;
+
+// Loads mobilenet and returns a model that returns the internal activation
+// we'll use as input to our classifier model.
+async function loadTruncatedMobileNet() {
+  const mobilenet = await tf.loadLayersModel(
+    "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json"
+  );
+
+  // Return a model that outputs an internal activation.
+  const layer = mobilenet.getLayer("conv_pw_13_relu");
+  return tf.model({ inputs: mobilenet.inputs, outputs: layer.output });
+}
+
+// When the UI buttons are pressed, read a frame from the webcam and associate
+// it with the class label given by the button. up, down, left, right are
+// labels 0, 1, 2, 3 respectively.
+ui.setExampleHandler(async label => {
+  let img = await getImage();
+
+  controllerDataset.addExample(truncatedMobileNet.predict(img), label);
+
+  // Draw the preview thumbnail.
+  ui.drawThumb(img, label);
+  img.dispose();
+});
+
+/**
+ * Sets up and trains the classifier.
+ */
+async function train() {
+  if (controllerDataset.xs == null) {
+    throw new Error("Add some examples before training!");
+  }
+
+  // Creates a 2-layer fully connected model. By creating a separate model,
+  // rather than adding layers to the mobilenet model, we "freeze" the weights
+  // of the mobilenet model, and only train weights from the new model.
+  model = tf.sequential({
+    layers: [
+      // Flattens the input to a vector so we can use it in a dense layer. While
+      // technically a layer, this only performs a reshape (and has no training
+      // parameters).
+      tf.layers.flatten({
+        inputShape: truncatedMobileNet.outputs[0].shape.slice(1)
+      }),
+      // Layer 1.
+      tf.layers.dense({
+        units: ui.getDenseUnits(),
+        activation: "relu",
+        kernelInitializer: "varianceScaling",
+        useBias: true
+      }),
+      // Layer 2. The number of units of the last layer should correspond
+      // to the number of classes we want to predict.
+      tf.layers.dense({
+        units: NUM_CLASSES,
+        kernelInitializer: "varianceScaling",
+        useBias: false,
+        activation: "softmax"
+      })
+    ]
+  });
+
+  // Creates the optimizers which drives training of the model.
+  const optimizer = tf.train.adam(ui.getLearningRate());
+  // We use categoricalCrossentropy which is the loss function we use for
+  // categorical classification which measures the error between our predicted
+  // probability distribution over classes (probability that an input is of each
+  // class), versus the label (100% probability in the true class)>
+  model.compile({ optimizer: optimizer, loss: "categoricalCrossentropy" });
+
+  // We parameterize batch size as a fraction of the entire dataset because the
+  // number of examples that are collected depends on how many examples the user
+  // collects. This allows us to have a flexible batch size.
+  const batchSize = Math.floor(
+    controllerDataset.xs.shape[0] * ui.getBatchSizeFraction()
+  );
+  if (!(batchSize > 0)) {
+    throw new Error(
+      `Batch size is 0 or NaN. Please choose a non-zero fraction.`
+    );
+  }
+
+  // Train the model! Model.fit() will shuffle xs & ys so we don't have to.
+  model.fit(controllerDataset.xs, controllerDataset.ys, {
+    batchSize,
+    epochs: ui.getEpochs(),
+    callbacks: {
+      onBatchEnd: async (batch, logs) => {
+        ui.trainStatus("Loss: " + logs.loss.toFixed(5));
+      }
+    }
+  });
+}
+
+let isPredicting = false;
+
+async function predict() {
+  ui.isPredicting();
+  while (isPredicting) {
+    // Capture the frame from the webcam.
+    const img = await getImage();
+
+    // Make a prediction through mobilenet, getting the internal activation of
+    // the mobilenet model, i.e., "embeddings" of the input images.
+    const embeddings = truncatedMobileNet.predict(img);
+
+    // Make a prediction through our newly-trained model using the embeddings
+    // from mobilenet as input.
+    const predictions = model.predict(embeddings);
+
+    // Returns the index with the maximum probability. This number corresponds
+    // to the class the model thinks is the most probable given the input.
+    const predictedClass = predictions.as1D().argMax();
+    const classId = (await predictedClass.data())[0];
+    img.dispose();
+
+    ui.predictClass(classId);
+    console.log(classId)
+    await tf.nextFrame();
+  }
+  ui.donePredicting();
+}
+
+/**
+ * Captures a frame from the webcam and normalizes it between -1 and 1.
+ * Returns a batched image (1-element batch) of shape [1, w, h, c].
+ */
+async function getImage() {
+  const img = await webcam.capture();
+  const processedImg = tf.tidy(() =>
+    img
+      .expandDims(0)
+      .toFloat()
+      .div(127)
+      .sub(1)
+  );
+  img.dispose();
+  return processedImg;
+}
+
+document.getElementById("train").addEventListener("click", async () => {
+  ui.trainStatus("Training...");
+  console.log("training");
+  tf.nextFrame();
+  tf.nextFrame();
+  isPredicting = false;
+  train();
+});
+document.getElementById("predict").addEventListener("click", () => {
+  console.log("predicting");
+  ui.startPacman();
+  isPredicting = true;
+  predict();
+});
+
+async function init() {
+  try {
+    webcam = await tfd.webcam(document.getElementById("webcam"));
+  } catch (e) {
+    console.log(e);
+    document.getElementById("no-webcam").style.display = "block";
+  }
+  truncatedMobileNet = await loadTruncatedMobileNet();
+
+  ui.init();
+
+  // Warm up the model. This uploads weights to the GPU and compiles the WebGL
+  // programs so the first time we collect data from the webcam it will be
+  // quick.
+  const screenShot = await webcam.capture();
+  truncatedMobileNet.predict(screenShot.expandDims(0));
+  screenShot.dispose();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const videoElement = document.querySelector('video');
+const audioInputSelect = document.querySelector('select#audioSource');
+const audioOutputSelect = document.querySelector('select#audioOutput');
+const videoSelect = document.querySelector('select#videoSource');
+const selectors = [audioInputSelect, audioOutputSelect, videoSelect];
+
+audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
+
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  const values = selectors.map(select => select.value);
+  selectors.forEach(select => {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+  });
+  for (let i = 0; i !== deviceInfos.length; ++i) {
+    const deviceInfo = deviceInfos[i];
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audioinput') {
+      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
+      audioInputSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'audiooutput') {
+      option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`;
+      audioOutputSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+      videoSelect.appendChild(option);
+    } else {
+      console.log('Some other kind of source/device: ', deviceInfo);
+    }
+  }
+  selectors.forEach((select, selectorIndex) => {
+    if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
+      select.value = values[selectorIndex];
+    }
+  });
+}
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+// Attach audio output device to video element using device/sink ID.
+function attachSinkId(element, sinkId) {
+  if (typeof element.sinkId !== 'undefined') {
+    element.setSinkId(sinkId)
+        .then(() => {
+          console.log(`Success, audio output device attached: ${sinkId}`);
+        })
+        .catch(error => {
+          let errorMessage = error;
+          if (error.name === 'SecurityError') {
+            errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`;
+          }
+          console.error(errorMessage);
+          // Jump back to first output device in the list as it's the default.
+          audioOutputSelect.selectedIndex = 0;
+        });
+  } else {
+    console.warn('Browser does not support output device selection.');
+  }
+}
+
+function changeAudioDestination() {
+  const audioDestination = audioOutputSelect.value;
+  attachSinkId(videoElement, audioDestination);
+}
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
+  console.log(`Success, Streaming`);
+  // Refresh button list in case labels have become available
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+function start() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+  const audioSource = audioInputSelect.value;
+  const videoSource = videoSelect.value;
+  const constraints = {
+    audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
+}
+
+audioInputSelect.onchange = start;
+audioOutputSelect.onchange = changeAudioDestination;
+
+videoSelect.onchange = start;
+
+start();
+
+
+
+
+
+
+// Initialize the application.
+init();
